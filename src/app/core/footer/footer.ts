8fe35fb98bd73ca76d9607481b1b1282434fd4b9
Qv1ko <vicgarmur947@gmail.com>
2026-07-28 18:30:16 +0200

    feat: add footer and header component files

---
+++ src/app/core/footer/footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qv1-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {}
---
+++ src/app/core/header/header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qv1-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {}
