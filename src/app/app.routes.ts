b23e48bdd7d6bb34531b90b52746f8dfeeb0e866
Qv1ko <vicgarmur947@gmail.com>
2026-07-31 21:46:04 +0200

    feat: add register page with routerLink parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./bookings/bookings'),
+    loadComponent: () => import('./bookings/bookings.page'),
   },
   {
     path: 'auth/login',
-    loadComponent: () => import('./routes/auth/login'),
+    loadComponent: () => import('./routes/auth/login/login.page'),
+  },
+  {
+    path: 'auth/register',
+    loadComponent: () => import('./routes/auth/register/register.page'),
   },
 ];
---
+++ src/app/routes/auth/register/register.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  imports: [RouterLink],
+  templateUrl: './register.page.html',
+  styleUrl: './register.page.css',
+})
+export default class RegisterPage {}
