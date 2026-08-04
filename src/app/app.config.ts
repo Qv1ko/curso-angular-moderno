21b1ae7265c378f0dc4db377a5288ae3269e32ce
Qv1ko <vicgarmur947@gmail.com>
2026-08-04 17:48:51 +0200

    feat: add api data using observables with subscribe

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
   ],
--- src/app/routes/home/home.page.ts
+++ src/app/routes/home/home.page.ts
@@ -4,6 +4,8 @@ import { Meta, Title } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 
 import { ACTIVITIES } from '../../domain/activities.data';
+import { Activity } from '../../domain/activity.type';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   imports: [RouterLink, CurrencyPipe, DatePipe],
@@ -11,13 +13,21 @@ import { ACTIVITIES } from '../../domain/activities.data';
   styleUrl: './home.page.css',
 })
 export default class HomePage {
+  private httpClient$: HttpClient = inject(HttpClient);
+  private readonly apiUrl = 'http://localhost:3000/activities';
+
   #title = inject(Title);
   #meta = inject(Meta);
 
-  activities = ACTIVITIES;
+  activities: Activity[] = [];
 
   constructor() {
     this.#title.setTitle('Activities to book');
     this.#meta.updateTag({ name: 'description', content: 'Book your favorite activities' });
+
+    this.httpClient$.get<Activity[]>(this.apiUrl).subscribe({
+      next: (activities) => this.activities = activities,
+      error: (err) => console.error(err),
+    });
   }
 }
