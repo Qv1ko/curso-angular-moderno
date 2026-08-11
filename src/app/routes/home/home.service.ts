633165a3964d69c6b71e8152ef318f3d989af30e
Qv1ko <vicgarmur947@gmail.com>
2026-08-11 19:14:24 +0200

    style: format code

--- src/app/routes/home/home.service.ts
+++ src/app/routes/home/home.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
-import { Activity } from '../../domain/activity.type';
 import { Observable } from 'rxjs';
+
+import { Activity } from '../../domain/activity.type';
 
 @Service()
 export class HomeService {
-    private httpClient$: HttpClient = inject(HttpClient);
-    private readonly apiUrl: string = 'http://localhost:3000/activities';
+  private httpClient$: HttpClient = inject(HttpClient);
+  private readonly apiUrl: string = 'http://localhost:3000/activities';
 
-    getActivities(): Observable<Activity[]> {
-        return this.httpClient$.get<Activity[]>(this.apiUrl);
-    }
+  getActivities(): Observable<Activity[]> {
+    return this.httpClient$.get<Activity[]>(this.apiUrl);
+  }
 }
