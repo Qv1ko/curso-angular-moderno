28a86ee7abc50f7da4de18452430682f5ff28c7c
Qv1ko <vicgarmur947@gmail.com>
2026-08-17 23:23:47 +0200

    feat: handle multiple simultaneous requests, monitoring times and results

--- src/app/routes/favorites/favorites.page.ts
+++ src/app/routes/favorites/favorites.page.ts
@@ -1,13 +1,28 @@
 import { Component, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
+import { ActivitiesRepository } from '@api/activities.repository';
+import { Activity } from '@domain/activity.type';
 import { FavoritesStore } from '@state/favorites.store';
+import { forkJoin, Observable } from 'rxjs';
 
 @Component({
   selector: 'qv1-favorites',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './favorites.page.html',
   styleUrl: './favorites.page.css',
 })
 export default class FavoritesPage {
   private favoritesStore: FavoritesStore = inject(FavoritesStore);
-  favorites: Signal<string[]> = this.favoritesStore.state;
+  private activitiesRepository: ActivitiesRepository = inject(ActivitiesRepository);
+  private favoritesSlugs: string[] = this.favoritesStore.state();
+
+  private getActivityBySlug$ = (favoriteSlug: string) =>
+    this.activitiesRepository.getActivityBySlug$(favoriteSlug);
+  private mapActivitiesFromSlugs$: Observable<Activity>[] = this.favoritesSlugs.map(
+    this.getActivityBySlug$,
+  );
+  private activities$: Observable<Activity[]> = forkJoin(this.mapActivitiesFromSlugs$);
+
+  activities: Signal<Activity[]> = toSignal(this.activities$, { initialValue: [] });
 }
