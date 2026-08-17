5a7c74d336dfbd12e971420c36ed14f4052fe4dc
Qv1ko <vicgarmur947@gmail.com>
2026-08-17 19:50:43 +0200

    refactor: create search input component with filter, debounce and untilChanged using pipes

--- src/app/shared/ui/filter/filter.widget.ts
+++ src/app/shared/ui/filter/filter.widget.ts
@@ -3,11 +3,12 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { DEFAULT_FILTER, Filter, SortOrders } from '@domain/filter.type';
+import { SearchComponent } from '@ui/search/search.component';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'qv1-filter',
-  imports: [FormsModule],
+  imports: [FormsModule, SearchComponent],
   templateUrl: './filter.widget.html',
   styleUrl: './filter.widget.css',
 })
@@ -19,9 +20,15 @@ export class FilterWidget {
     initialValue: DEFAULT_FILTER,
   });
 
-  search: WritableSignal<string> = signal<string>(this.defaultFilter().search);
-  orderBy: WritableSignal<string> = signal<string>(this.defaultFilter().orderBy);
-  sort: WritableSignal<SortOrders> = signal<SortOrders>(this.defaultFilter().sort);
+  search: WritableSignal<string> = signal<string>(
+    this.defaultFilter().search || DEFAULT_FILTER.search,
+  );
+  orderBy: WritableSignal<string> = signal<string>(
+    this.defaultFilter().orderBy || DEFAULT_FILTER.orderBy,
+  );
+  sort: WritableSignal<SortOrders> = signal<SortOrders>(
+    this.defaultFilter().sort || DEFAULT_FILTER.sort,
+  );
 
   private filter = computed(() => ({
     search: this.search(),
---
+++ src/app/shared/ui/search/search.component.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  effect,
+  ElementRef,
+  model,
+  ModelSignal,
+  Signal,
+  viewChild,
+} from '@angular/core';
+import { debounce, debounceTime, distinctUntilChanged, filter, fromEvent, map, tap } from 'rxjs';
+
+@Component({
+  selector: 'qv1-search',
+  imports: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {
+  private searchInput: Signal<ElementRef | undefined> = viewChild('searchInput', {
+    read: ElementRef,
+  });
+  searchTerm: ModelSignal<string> = model<string>('');
+
+  constructor() {
+    effect(() => {
+      const input = this.searchInput();
+      if (!input) return;
+      fromEvent<Event>(input.nativeElement, 'input')
+        .pipe(
+          map((event: Event) => (event.target as HTMLInputElement).value),
+          filter((value: string) => value.length > 2 || value.length === 0),
+          debounceTime(300),
+          distinctUntilChanged(),
+        )
+        .subscribe((term: string) => this.searchTerm.set(term));
+    });
+  }
+}
